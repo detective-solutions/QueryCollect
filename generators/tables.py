94002234d5520495659c5844120d6ee252d7c641
Maxim Perl <maxim.perl@outlook.de>
2021-05-15 13:28:51 +0200

    refactor(DET-519): add random values disclaimer in base.html, fix(DET-519): fix the row_filter column

--- generators/tables.py
+++ generators/tables.py
@@ -6,9 +6,7 @@ import operator
 # import third party modules
 import numpy as np
 import pandas as pd
-
-# import project related modules
-from generators.dataset import DataSetGenerator
+from randomtables import DataSetGenerator
 
 
 class QueryTable:
@@ -91,13 +89,22 @@ class QueryTable:
             ],
         )
 
-        # select a random column of both and select
+        str_operations = [operator.eq, operator.ne]
+        num_operations = [operator.lt, operator.le, operator.ge, operator.gt] + str_operations
+
+        # select a random column to pick a random value to filter for
         columns = input_data.columns.tolist()
         column_to_select = random.choice(columns)
-        new_name = self.data_generator.column_name_generator(d_type="<class 'int'>")
+        filter_value = random.choice(input_data[column_to_select].tolist())
+
+        column_dtype = input_data.loc[:, [column_to_select]].dtypes
+        column_dtype = str(list(column_dtype)[0])
+
+        operations = num_operations if column_dtype in ["float64", "int32"] else str_operations
 
         # create an output dataset
-        output_data = input_data.loc[:, columns].rename(columns={column_to_select: new_name})
+        row_filter = random.choice(operations)
+        output_data = input_data.loc[row_filter(input_data[column_to_select], filter_value)]
 
         return input_data.to_dict("records"), output_data.to_dict("records")
 
@@ -469,3 +476,10 @@ class QueryTable:
         :return: tuple with lists of records for input, output
         """
         return self.query_type[q_type]()
+
+
+qt = QueryTable()
+i, o = qt.query_task(0)
+print(i)
+
+print(o)
